662546d6945e44a0f3c2f64548e018ff0349c732
Ibekwe Prince <zuvec00@gmail.com>
2024-11-27 21:29:16 +0100

    Enhance Forgot Password screen: added input validation functionality

--- lib/views/screens/auth/forgot_password_screen.dart
+++ lib/views/screens/auth/forgot_password_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -9,13 +11,60 @@ import 'package:stitches_africa/views/components/custom_textfield.dart';
 import 'package:stitches_africa/views/components/toastification.dart';
 import 'package:toastification/toastification.dart';
 
-class ForgotPasswordScreen extends StatelessWidget {
+class ForgotPasswordScreen extends StatefulWidget {
   ForgotPasswordScreen({super.key});
 
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
   final FirebaseAuthService firebaseAuthService = FirebaseAuthService();
+
   final ShowToasitification showToasitification = ShowToasitification();
+
   final TextEditingController forgotPasswordController =
       TextEditingController();
+
+  // Error variables
+  String? forgotPasswordError;
+
+  bool _validateFields() {
+    setState(() {
+      forgotPasswordError = forgotPasswordController.text.isEmpty
+          ? 'This field is required'
+          : null;
+    });
+    // Return true if all fields are filled, otherwise false
+    return forgotPasswordError == null;
+  }
+
+  void _showLoadingDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return const Center(
+          child: CircularProgressIndicator(color: Utilities.backgroundColor),
+        );
+      },
+    );
+  }
+
+  Future<void> _forgotPasswordHandler() async {
+    if (_validateFields()) {
+      _showLoadingDialog();
+      await firebaseAuthService
+          .resetPassword(forgotPasswordController.text.trim());
+      context.pop();
+      context.pop();
+      showToasitification.showToast(
+        context: context,
+        toastificationType: ToastificationType.success,
+        title: 'Password reset link sent successfully',
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,13 +89,25 @@ class ForgotPasswordScreen extends StatelessWidget {
         child: Center(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
+            // mainAxisAlignment: MainAxisAlignment.,
             children: [
+              Text(
+                'STITCHES AFRICA',
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontSize: 24.sp,
+                  letterSpacing: 1,
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+              SizedBox(
+                height: 225.h,
+              ),
               Text(
                 'Forgot Password?',
                 style: TextStyle(
                     fontFamily: 'Montserrat',
-                    fontSize: 24.sp,
+                    fontSize: 22.sp,
                     fontWeight: FontWeight.w600),
               ),
               SizedBox(
@@ -67,29 +128,18 @@ class ForgotPasswordScreen extends StatelessWidget {
                 controller: forgotPasswordController,
                 hintText: 'EMAIL',
                 obscureText: false,
+                errorText: forgotPasswordError,
               ),
               SizedBox(
                 height: 25.h,
               ),
               Button(
-                  border: false,
-                  text: 'Reset Password',
-                  onTap: () async {
-                    showDialog(
-                        context: context,
-                        builder: (context) {
-                          return const Center(
-                              child: CircularProgressIndicator(
-                                  color: Utilities.backgroundColor));
-                        });
-                    await firebaseAuthService
-                        .resetPassword(forgotPasswordController.text.trim());
-                    Navigator.pop(context);
-                    showToasitification.showToast(
-                        context: context,
-                        toastificationType: ToastificationType.success,
-                        title: 'Password reset link sent successfully');
-                  })
+                border: false,
+                text: 'Reset Password',
+                onTap: () async {
+                  _forgotPasswordHandler();
+                },
+              )
             ],
           ),
         ),
